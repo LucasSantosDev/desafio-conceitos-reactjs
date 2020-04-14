6bea53a4d1c4c52543bab9147e0d69aeba8fd61f
Lucas Dev <lucas_ss.arts@live.com>
2020-04-14 15:11:33 -0300

    (feature) challenge finished

---
+++ README.md
@@ -0,0 +1,27 @@
+## Desafio 03: Conceitos do ReactJS
+
+👌 Feito!
+
+🙌 Todos os testes passaram!
+
+🚀🚀🚀 Rocketseat!
+
+### Rodando Project
+
+```
+yarn install
+```
+
+<small>\*Para instalar todos os pacotes</small>
+
+```
+yarn run start
+```
+
+<small>\*Para rodar o projeto na porta especificada no codigo (3000)</small>
+
+```
+yarn run test
+```
+
+<small>\*Para rodar todos os testes</small>
--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repository, setRepository] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await api.get("repositories");
+
+        if (data && data.length > 0) {
+          setRepository(data);
+        }
+      } catch (error) {
+        console.error("Exception > ", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const data = {
+      title: `Novo projeto teste-${new Date()}`,
+      url: `http://${new Date()}.com.br`,
+    };
+
+    try {
+      const { data: responseData } = await api.post("repositories", data);
+
+      setRepository([...repository, responseData]);
+    } catch (error) {
+      console.error("Exception > ", error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+
+      const repositoryIndex = repository.findIndex(
+        (repository) => repository.id === id
+      );
+
+      repository.splice(repositoryIndex, 1);
+
+      setRepository([]);
+      setRepository(repository);
+    } catch (error) {
+      console.error("Exception > ", error);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repository &&
+          repository.map((r) => (
+            <li key={`${r.id}`}>
+              {r.title}
+              <button onClick={() => handleRemoveRepository(r.id)}>
+                Remover
+              </button>
+            </li>
+          ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
